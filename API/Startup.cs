8a6b874ebf075e92c73193ef15c041e4176f32b7
Merna Meawad <MMeawad@path-solutions.com>
2022-10-11 22:31:20 +0200

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -24,7 +24,7 @@ namespace API
             _config = config;
         }
 
- 
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -34,6 +34,7 @@ namespace API
             });
 
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -53,6 +54,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
